a5a106c4981d6658bb938978e9e8280d229e276a
dgladkov96 <daniilg1996@gmail.com>
2022-04-26 20:14:55 -0400

    SeleniumDriver+Web

---
+++ src/test/java/Web/MyDriver.java
@@ -0,0 +1,30 @@
+package Web;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MyDriver {
+
+    private static WebDriver driver;
+
+    public static void launchUrlOnNewWindow(String url) {
+        System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver");
+        driver = new ChromeDriver();
+
+        driver.get(url);
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            driver.quit();
+        }
+    }
+
+    public static void quitWindows() {
+        driver.quit();
+    }
+    public static WebDriver getDriver() {
+        return driver;
+
+    }
+}
