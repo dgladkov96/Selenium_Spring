af29dfd322edbfbfe40926bad771c66d904efdf5
dgladkov96 <daniilg1996@gmail.com>
2022-04-28 19:06:05 -0400

    SeleniumDriver+Web

---
+++ src/test/java/SeleniumHW/Seleniumhw2.java
@@ -0,0 +1,10 @@
+package SeleniumHW;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Seleniumhw2 {
+}
